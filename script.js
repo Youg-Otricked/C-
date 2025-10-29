2e4505e70c6b9a3ac0a65b37f4ab59ceaf4c9df1
youg <yougotricked2@gmail.com>
2025-10-28 20:45:36 -0700

    done with C++

---
+++ script.js
@@ -0,0 +1,55 @@
+function correct() {
+    console.log("corect");
+    let wintext = document.getElementById("win");
+    wintext.textContent = "Correct!"
+    setTimeout(() => {
+        wintext.textContent = ""
+        alert("Correct!!!")
+
+        let nls = document.getElementById("nl");
+        nls.textContent = "Next Lesson?"
+        nls.classList.add('active');
+    }, 500);
+}
+function incorrect() {
+    let nls = document.getElementById("nl");
+    nls.textContent = ""
+    nls.classList.remove('active');
+    console.log("Incorrect");
+
+    let failtxt = document.getElementById("fail");
+    failtxt.textContent = "Incorrect."
+    setTimeout(() => {
+        failtxt.textContent = ""
+        alert("Incorrect")
+
+    }, 500);
+}
+function submit() {
+    let input = document.getElementById("i");
+    if (input.value == 'std::cout << "Hello, World!";') {
+        console.log("corect");
+        let wintext = document.getElementById("win");
+        wintext.textContent = "Correct!"
+        setTimeout(() => {
+            wintext.textContent = ""
+            alert("Correct!!!")
+
+            let nls = document.getElementById("nl");
+            nls.textContent = "Next Lesson?"
+            nls.classList.add('active');
+        }, 500);
+    } else {
+        let nls = document.getElementById("nl");
+        nls.textContent = ""
+        nls.classList.remove('active');
+        console.log("Incorrect");
+        let failtxt = document.getElementById("fail");
+        failtxt.textContent = "Incorrect."
+        setTimeout(() => {
+            failtxt.textContent = ""
+            alert("Incorrect")
+
+        }, 500);
+    }
+}
