7ef89a86dd75fa08d42e1803f057d38fca6b2e5e
youg <yougotricked2@gmail.com>
2025-10-29 16:52:30 -0700

    Done

--- script.js
+++ script.js
@@ -53,3 +53,31 @@ function submit() {
         }, 500);
     }
 }
+function submit2() {
+    let input = document.getElementById("i");
+    if (input.value == 'Console.WriteLine("Hello, World!");') {
+        console.log("corect");
+        let wintext = document.getElementById("win");
+        wintext.textContent = "Correct!"
+        setTimeout(() => {
+            wintext.textContent = ""
+            alert("Correct!!!")
+
+            let nls = document.getElementById("nl");
+            nls.textContent = "Next Lesson?"
+            nls.classList.add('active');
+        }, 500);
+    } else {
+        let nls = document.getElementById("nl");
+        nls.textContent = ""
+        nls.classList.remove('active');
+        console.log("Incorrect");
+        let failtxt = document.getElementById("fail");
+        failtxt.textContent = "Incorrect."
+        setTimeout(() => {
+            failtxt.textContent = ""
+            alert("Incorrect")
+
+        }, 500);
+    }
+}
